7bdb0784b6c964267969efdc3966b48b100c5aa7
dennis-del <dennisjames991@gmail.com>
2025-02-13 13:45:42 +0530

    some minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/assign_calc"> {/* Add basename here */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
